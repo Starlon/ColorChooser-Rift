ec1b2526ec981baa2067016900fe7347479a1ffc
Scott Sibley <sisibley@gmail.com>
2011-12-05 20:59:51 -0600

    Try to resize it and provide slash command to show an example color chooser.

--- ColorChooser.lua
+++ ColorChooser.lua
@@ -1,6 +1,7 @@
 Library = Library or {}
 Library.ColorChooser = {}
 
+local DEFAULT_PIXEL = 5
 
 --- Return RGB values from HSV colorspace values.
 -- @usage HSV2RGB(h, s, v)
@@ -60,24 +61,51 @@ local function HSV2RGB(h, s, v)
 	return r, g, b
 end
 
+local widget
 Library.ColorChooser.CreateWidget = function(frame, handler, pixel)
 	local r, g, b, a = 0, 0, 0, 1
 	local pixel = pixel or 5
 	frame.textures = frame.textures or {}
 
 	local x, y = 0, 0
-	local count = 0
-	for h = 0, 360, 360/42 do
-		for v = 99, 0, -(100/8) do
-			local s = 100
+	local count = 5
+	local draw = function(pixel)
+		for h = 0, 360, 360/33 do
+			for v = 99, 0, -(100/8) do
+				local s = 100
+				y = count * pixel
+				count = count + 1
+					
+				local texture = select(2, table.remove(frame.textures)) or UI.CreateFrame("Frame", "Pixel", frame)
+				texture:ClearAll()
+				texture:SetMouseMasking("full")	
+				texture:SetPoint("TOPLEFT", frame, "TOPLEFT", x, y)
+			
+				local r, g, b = HSV2RGB(h/360, s/100, v/100)
+				texture:SetBackgroundColor(r, g, b)
+				texture:SetWidth(pixel)
+				texture:SetHeight(pixel)
+		
+				texture.Event.LeftClick = function()
+					handler(r, g, b)
+				end
+				table.insert(frame.textures, texture)
+			end
+			x = x + pixel
+			count = 0
+		end
+		count = 0
+		for v = 99, 0, -(100/8)  do
+			local h, s, v = 0, 0, v
+	
 			y = count * pixel
 			count = count + 1
-				
-			local texture = UI.CreateFrame("Frame", "Pixel", frame)
+	
+			local texture = select(2, table.remove(frame.textures)) or UI.CreateFrame("Frame", "Pixel", frame)
 			texture:ClearAll()
-			texture:SetMouseMasking("full")	
+			texture:SetMouseMasking("full")
 			texture:SetPoint("TOPLEFT", frame, "TOPLEFT", x, y)
-		
+			
 			local r, g, b = HSV2RGB(h/360, s/100, v/100)
 			texture:SetBackgroundColor(r, g, b)
 			texture:SetWidth(pixel)
@@ -88,31 +116,37 @@ Library.ColorChooser.CreateWidget = function(frame, handler, pixel)
 			end
 			table.insert(frame.textures, texture)
 		end
-		x = x + pixel
-		count = 0
 	end
-	count = 0
-	for v = 99, 0, -(100/8)  do
-		local h, s, v = 0, 0, v
-
-		y = count * pixel
-		count = count + 1
-
-		local texture = UI.CreateFrame("Frame", "Pixel", frame)
-		texture:ClearAll()
-		texture:SetMouseMasking("full")
-		texture:SetPoint("TOPLEFT", frame, "TOPLEFT", x, y)
-		
-		local r, g, b = HSV2RGB(h/360, s/100, v/100)
-		texture:SetBackgroundColor(r, g, b)
-		texture:SetWidth(pixel)
-		texture:SetHeight(pixel)
-
-		texture.Event.LeftClick = function()
-			handler(r, g, b)
-		end
-		table.insert(frame.textures, texture)
+	frame.ResizePixel = function(self, pixel)
+		pixel = pixel or DEFAULT_PIXEL
+		count, x, y = pixel, 0, 0
+		draw(pixel)
 	end
+	frame:ResizePixel(DEFAULT_PIXEL)
 	return frame
 end
+
+local ctx = UI.CreateContext("ColorChooser")
+local frame
+table.insert(Command.Slash.Register("colorchooser"), {function (commands)	
+
+	local show = commands:match("^show")
+	local resize = commands:match("^resize (%d+)")
+	if show then
+		frame = UI.CreateFrame("Frame", "ColorChooser", ctx)
+		frame:SetPoint("CENTER", UIParent, "CENTER")
+		frame:SetVisible(true)
+		local pixel = 8
+		local handler = function(r, g, b)
+			print(string.format("%02x%02x%02x", r*256, g*256, b*256))
+		end
+		local cc = Library.ColorChooser.CreateWidget(frame, handler, pixel)
+	elseif resize then
+		if frame then 
+			frame:SetVisible(false)
+			frame:ResizePixel(tonumber(resize))
+		end
+	end
+
+end, "ColorChooser", "Slash"})
 
